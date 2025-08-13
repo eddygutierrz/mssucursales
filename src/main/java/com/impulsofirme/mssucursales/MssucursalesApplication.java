1770376c4aea4b566b58f6f57970edc4ca7dc279
eddy.gutierrez <eddy.gutierrez@remtools.mx>
2025-08-13 13:24:23 -0600

    Primer commit - estructura inicial

---
+++ src/main/java/com/impulsofirme/mssucursales/MssucursalesApplication.java
@@ -0,0 +1,13 @@
+package com.impulsofirme.mssucursales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssucursalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssucursalesApplication.class, args);
+	}
+
+}
