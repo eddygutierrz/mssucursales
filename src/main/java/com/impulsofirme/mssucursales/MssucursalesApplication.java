d55e442c0eb3bcdf5fcac9ada8be1e97309b26e3
eddy.gutierrez <eddy.gutierrez@remtools.mx>
2025-08-13 21:31:32 -0600

    Ajustes de buenas practicas

--- src/main/java/com/impulsofirme/mssucursales/MssucursalesApplication.java
+++ src/main/java/com/impulsofirme/mssucursales/MssucursalesApplication.java
@@ -2,10 +2,14 @@ package com.impulsofirme.mssucursales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.impulsofirme.mssucursales.config.JwtProperties;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableConfigurationProperties(JwtProperties.class)
 public class MssucursalesApplication {
 
 	public static void main(String[] args) {
